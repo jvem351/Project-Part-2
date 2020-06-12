2776b6734aa7d11a5b0d5bf71eee584e46bfa0f7
jvem351 <64933774+jvem351@users.noreply.github.com>
2020-06-12 19:19:48 +1200

    192 Project part 2

---
+++ script.js
@@ -0,0 +1,6 @@
+const carouselSlide = document.querySelector(".carousel-slide img");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+const Prev = document.querySelector("#prev");
+const Next = document.querySelector("#next");
+
